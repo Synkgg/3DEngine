110750bd8980e1045d13f60bad88dabe35f7f664
fezqi <166904880+Synkgg@users.noreply.github.com>
2026-09-22 03:10:52 -0400

    Add material and local light renderer API

--- Engine/Graphics/Renderer.h
+++ Engine/Graphics/Renderer.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory>
+#include <array>
 
 #include <glad/gl.h>
 #include <SDL3/SDL.h>
@@ -21,6 +22,9 @@
 
 class Window;
 class Texture2D;
+
+struct PointLightData { Vec3 position; Vec3 color; float intensity=1.0f; float range=10.0f; };
+struct SpotLightData { Vec3 position; Vec3 direction; Vec3 color; float intensity=1.0f; float range=15.0f; float innerCos=0.92f; float outerCos=0.82f; };
 
 class Renderer
 {
@@ -44,7 +48,11 @@ public:
         float green,
         float blue,
         float alpha,
-        const Texture2D* texture = nullptr
+        const Texture2D* texture = nullptr,
+        float metallic = 0.0f,
+        float roughness = 0.65f,
+        float ambientOcclusion = 1.0f,
+        float emissive = 0.0f
     );
 
     SDL_GLContext GetContext() const;
@@ -72,6 +80,9 @@ public:
     Vec3 GetCameraRayDirection(float ndcX, float ndcY) const;
 
     void SetDirectionalLight(const Vec3& direction, const Vec3& color, float intensity);
+    void ClearLocalLights();
+    void AddPointLight(const PointLightData& light);
+    void AddSpotLight(const SpotLightData& light);
     void DrawDirectionalLight(const Vec3& position, const Vec3& direction);
     void DrawCollider(const Transform& transform, float width, float height, float depth);
 
@@ -112,6 +123,10 @@ private:
     Vec3 m_LightDirection;
     Vec3 m_LightColor;
     float m_LightIntensity;
+    std::array<PointLightData, 8> m_PointLights{};
+    std::array<SpotLightData, 4> m_SpotLights{};
+    int m_PointLightCount = 0;
+    int m_SpotLightCount = 0;
 
     DebugRenderer m_DebugRenderer;
 
