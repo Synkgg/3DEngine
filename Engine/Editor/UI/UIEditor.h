10d5566d297827efa407af1e4c5452fff2e9a06a
fezqi <166904880+Synkgg@users.noreply.github.com>
2026-09-22 02:12:25 -0400

    Give UI editor renderer access and selected asset state

--- Engine/Editor/UI/UIEditor.h
+++ Engine/Editor/UI/UIEditor.h
@@ -4,16 +4,22 @@
 #include "../../UI/UILayout.h"
 
 #include <imgui.h>
+#include <string>
+
+class Renderer;
 
 class UIEditor
 {
 public:
     void Draw(
-        UICanvas& canvas
+        UICanvas& canvas,
+        Renderer& renderer
     );
 
 private:
     UIWidget* m_SelectedWidget = nullptr;
+    Renderer* m_Renderer = nullptr;
+    std::string m_UIAssetPath = "Assets/UI/Main.ui";
 
     bool m_Dragging = false;
     bool m_Resizing = false;
