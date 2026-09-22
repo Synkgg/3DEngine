fd22233754497f1d33e52109789aa5cb80a39ecb
fezqi <166904880+Synkgg@users.noreply.github.com>
2026-09-22 01:16:29 -0400

    UI rewrite: introduce UMG-style widget layout core

--- Engine/UI/UILayout.cpp
+++ Engine/UI/UILayout.cpp
@@ -1,43 +1,45 @@
 #include "UILayout.h"
 
+#include <algorithm>
+
 UIRect UILayout::Calculate(
     const UIWidget& widget,
     const UIRect& parentRect)
 {
-    const Vec2 position =
-        widget.GetPosition();
+    const Vec2 anchorMin = widget.GetAnchorMinimum();
+    const Vec2 anchorMax = widget.GetAnchorMaximum();
+    const Vec2 position = widget.GetPosition();
+    const Vec2 size = widget.GetSize();
+    const Vec2 pivot = widget.GetPivot();
 
-    const Vec2 size =
-        widget.GetSize();
-
-    const Vec2 anchor =
-        widget.GetAnchor();
-
-    const Vec2 pivot =
-        widget.GetPivot();
-
-    const float anchorX =
-        parentRect.x +
-        parentRect.width * anchor.x;
-
-    const float anchorY =
-        parentRect.y +
-        parentRect.height * anchor.y;
+    const float minX = parentRect.x + parentRect.width * anchorMin.x;
+    const float minY = parentRect.y + parentRect.height * anchorMin.y;
+    const float maxX = parentRect.x + parentRect.width * anchorMax.x;
+    const float maxY = parentRect.y + parentRect.height * anchorMax.y;
 
     UIRect result;
 
-    result.width = size.x;
-    result.height = size.y;
+    if (anchorMin.x == anchorMax.x)
+    {
+        result.width = size.x;
+        result.x = minX + position.x - result.width * pivot.x;
+    }
+    else
+    {
+        result.x = minX + position.x;
+        result.width = std::max(0.0f, (maxX - minX) - position.x - size.x);
+    }
 
-    result.x =
-        anchorX +
-        position.x -
-        size.x * pivot.x;
-
-    result.y =
-        anchorY +
-        position.y -
-        size.y * pivot.y;
+    if (anchorMin.y == anchorMax.y)
+    {
+        result.height = size.y;
+        result.y = minY + position.y - result.height * pivot.y;
+    }
+    else
+    {
+        result.y = minY + position.y;
+        result.height = std::max(0.0f, (maxY - minY) - position.y - size.y);
+    }
 
     return result;
 }
