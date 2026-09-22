7e5f6148f8b26c977e1858655d5f469574afdd20
fezqi <166904880+Synkgg@users.noreply.github.com>
2026-09-22 02:59:08 -0400

    Add Crystal Courtyard objective and win loop

--- Assets/Scripts/GameController.lua
+++ Assets/Scripts/GameController.lua
@@ -3,6 +3,11 @@ local inventoryOpen = false
 function OnCreate()
     UI.Load("Assets/UI/GameHUD.ui")
     UI.SetVisible("Inventory", false)
+    UI.SetVisible("WinScreen", false)
+
+    if CrystalGame then
+        CrystalGame.RefreshUI()
+    end
 end
 
 function OnUpdate(deltaTime)
@@ -13,5 +18,18 @@ function OnUpdate(deltaTime)
 
     if Input.IsKeyPressed("M") then
         Scene.Load("Assets/Scenes/MainMenu.scene")
+        return
+    end
+
+    if CrystalGame and CrystalGame.won then
+        if UI.WasClicked("PlayAgainButton") then
+            Scene.Load("Assets/Scenes/CrystalCourtyard.scene")
+            return
+        end
+
+        if UI.WasClicked("MainMenuButton") then
+            Scene.Load("Assets/Scenes/MainMenu.scene")
+            return
+        end
     end
 end
