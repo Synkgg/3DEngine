09cf2c8299151d77802deab73818770895292c21
fezqi <166904880+Synkgg@users.noreply.github.com>
2026-09-22 03:17:27 -0400

    Allow mesh components to reference imported models

--- Engine/Scene/Components/MeshComponent.h
+++ Engine/Scene/Components/MeshComponent.h
@@ -1,10 +1,13 @@
 #pragma once
 
 #include "../../Graphics/PrimitiveType.h"
+#include <string>
 
 struct MeshComponent
 {
     PrimitiveType primitive = PrimitiveType::None;
+    // Optional external model. OBJ is supported by the built-in loader.
+    std::string modelPath;
 
     Vec3 offset = Vec3(0.0f, 0.0f, 0.0f);
     Vec3 rotation = Vec3(0.0f, 0.0f, 0.0f);
