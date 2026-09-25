d2beff6b77993ee291f80895b19d916f79c0eda5
fezqi <166904880+Synkgg@users.noreply.github.com>
2026-09-24 22:50:52 -0400

    Replace crystal objective with cemetery keys

--- Assets/Scripts/CrystalPickup.lua
+++ Assets/Scripts/CrystalPickup.lua
@@ -1,19 +1,18 @@
 local collected = false
 
 function OnCreate()
-    Interactable.SetPrompt("Collect energy crystal")
+    Interactable.SetPrompt("Take cemetery key")
 end
 
 function OnUpdate(deltaTime)
     if not collected then
         local rotation = transform.GetRotation()
-        transform.SetRotation(rotation.x, rotation.y + (70.0 * deltaTime), rotation.z)
+        transform.SetRotation(rotation.x, rotation.y + (24.0 * deltaTime), rotation.z)
     end
 end
 
 function OnInteract()
     if collected then return end
-
     if CrystalGame and CrystalGame.Collect() then
         collected = true
         Collider.SetEnabled(false)
