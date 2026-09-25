c699be4adccde7298d614ad525c5ea9409aabf73
fezqi <166904880+Synkgg@users.noreply.github.com>
2026-09-24 23:20:39 -0400

    Add prefab instance lifecycle API

--- Engine/Scene/PrefabSerializer.h
+++ Engine/Scene/PrefabSerializer.h
@@ -2,6 +2,8 @@
 
 #include "Entity.h"
 #include <string>
+#include <unordered_map>
+#include <cstdint>
 
 class Scene;
 
@@ -10,4 +12,19 @@ class PrefabSerializer
 public:
     static bool Save(Scene& scene, Entity root, const std::string& filepath);
     static Entity Instantiate(Scene& scene, const std::string& filepath, Entity parent = Entity());
+
+    static bool IsInstanceRoot(const Scene& scene, Entity entity);
+    static std::string GetSource(const Scene& scene, Entity entity);
+    static bool Apply(Scene& scene, Entity instanceRoot);
+    static bool Revert(Scene& scene, Entity instanceRoot);
+    static bool Unpack(Scene& scene, Entity instanceRoot, bool completely = true);
+    static void ForgetEntity(const Scene& scene, Entity entity);
+    static void ForgetScene(const Scene& scene);
+
+private:
+    struct InstanceInfo
+    {
+        std::string source;
+    };
+    static std::unordered_map<const Scene*, std::unordered_map<std::uint32_t, InstanceInfo>> s_Instances;
 };
